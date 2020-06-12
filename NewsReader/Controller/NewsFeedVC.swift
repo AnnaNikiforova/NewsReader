224ec4bfa94efded93719edd03a8d7a74aab7116
Anna Nikiforova <annanikiforova19@mail.ru>
2020-06-12 18:06:51 +0300

    Revert "Revert "implemented Search Bar""
    This reverts commit f7015b52eb5f19a02b3891f9624263ed37268a71.

--- NewsReader/Controller/NewsFeedVC.swift
+++ NewsReader/Controller/NewsFeedVC.swift
@@ -11,20 +11,25 @@ import UIKit
 class NewsFeedVC: UIViewController {
     
     private var rssItems: [RSSItem]?
+    private var searchThroughRSSItems = [RSSItem?]()
+    
+    var isSearching = false
     
     let myRefreshControl: UIRefreshControl = {
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refresh(sender:)), for: .valueChanged)
         return refreshControl
     }()
-
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         fetchData()
         tableView.refreshControl = myRefreshControl
-      
+        
     }
     
     private func fetchData() {
@@ -42,27 +47,70 @@ class NewsFeedVC: UIViewController {
         fetchData()
         sender.endRefreshing()
     }
-
+    
 }
 
 // MARK: - TableView
 
 extension NewsFeedVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let rssItems = rssItems else {
-            return 0
+        
+        if isSearching {
+            return searchThroughRSSItems.count
+        } else {
+            guard let rssItems = rssItems else {
+                return 0
+            }
+            return rssItems.count
         }
-        return rssItems.count
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath) as! NewsCell
         
-        if let item = rssItems?[indexPath.item] {
-            cell.item = item
+        if isSearching {
+            if let item = searchThroughRSSItems[indexPath.item] {
+                cell.item = item
+            }
+            return cell
+        } else {
+            if let item = rssItems?[indexPath.item] {
+                cell.item = item
+            }
+            return cell
         }
-        return cell
     }
     
 }
 
+// MARK: - Search Bar
+
+extension NewsFeedVC: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text == nil || searchBar.text == "" {
+            isSearching = false
+            view.endEditing(true)
+            tableView.reloadData()
+        } else {
+            isSearching = true
+            searchThroughRSSItems = rssItems!.filter({ value -> Bool in
+                guard let text = searchBar.text?.lowercased() else { return false}
+                return value.title.lowercased().contains(text)
+            })
+            tableView.reloadData()
+        }
+        
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        searchBar.endEditing(true)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+    }
+    
+}
